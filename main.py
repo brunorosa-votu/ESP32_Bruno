80ea4ea5c599126c81ae1c0aa4908c96ee922dcb
brunorosa-votu <bruno.f.rosa6@aluno.senai.br>
2026-02-06 09:46:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,24 @@ amarelo.off()                   #       todos os leds       #
 vermelho.off()                  #        apagados           #
 
 while True: 
+    contador = 0                # Cria uma variável chamada "contador" e iguala a 0 #
+
     vermelho.off()              #   Apaga o led vermelho    #
+
+    print("VERDE")              #   Exibe "VERDE" no terminal    #
     verde.on()                  #   Acende o led verde      #
     sleep(10)                   #   Espera 10 segundos      #
 
     verde.off()                 #   Apaga o led verde       #
-    amarelo.on()                #   Acende o led amarelo    #
-    sleep(4)                    #   Espera 4 segundos       #
 
-    amarelo.off()               #   Apaga o led amarelo     #
+    print("AMARELO")            #   Exibe "AMARELO" no terminal  #
+    while contador < 20:        #    Enquanto "contador"    #
+        amarelo.on()            #     for menor que 20      #
+        sleep(0.1)              #      o led amarelo        #
+        amarelo.off()           #        vai piscar         #
+        sleep(0.1)              #     totalizando no fim    #
+        contador += 1           #   um tempo de 4 segundos  #
+          
+    print("VERMELHO")           #   Exibe "VERMELHO" no terminal #
     vermelho.on()               #   Acende o led vermelho   #
     sleep(10)                   #   Espera 10 segundos      #
